fb0d591345339f4eb2ea502447133a1930d621ad
Orhan Demirayak <orhan.demirayak@titra.com.tr>
2026-03-17 15:11:48 +0300

    #11

--- src/Ulak.API/Program.cs
+++ src/Ulak.API/Program.cs
@@ -3,6 +3,8 @@ using Serilog;
 using Serilog.Sinks.Elasticsearch;
 using Ulak.Api.Middleware;
 using Ulak.Persistence.Context;
+using Microsoft.OpenApi.Models;
+using System.Reflection;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -66,7 +68,37 @@ builder.Logging.AddConsole();
 builder.Logging.AddDebug();
 builder.Logging.AddConfiguration(builder.Configuration.GetSection("Logging"));
 
+builder.Services.AddEndpointsApiExplorer();
+
+builder.Services.AddSwaggerGen(options =>
+{
+    options.SwaggerDoc("v1", new OpenApiInfo
+    {
+        Title = "Ulak API",
+        Version = "v1",
+        Description = "Ulak backend API dokümantasyonu"
+    });
+
+    var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+    var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
+
+    if (File.Exists(xmlPath))
+    {
+        options.IncludeXmlComments(xmlPath);
+    }
+});
+
 var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI(options =>
+    {
+        options.SwaggerEndpoint("/swagger/v1/swagger.json", "Ulak API v1");
+        options.RoutePrefix = "swagger";
+    });
+}
 
 // Middleware sırası: Logging -> Exception -> HTTPS -> Controllers
 app.UseCorrelationId();
